069013dccd843c5702a26e459446d9af6937e37f
thaihoc285 <thaihoc131005@gmail.com>
2026-05-19 23:52:37 +0700

    refactor: restructure Spark operator DAG to use TaskGroup for better organization

--- airflow/dags/test_spark_operator.py
+++ airflow/dags/test_spark_operator.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.utils.task_group import TaskGroup
 
-APP_NAME = "spark-pi-test"
 
 with DAG(
     dag_id="test_spark_operator",
@@ -14,26 +14,25 @@ with DAG(
     tags=["pizza-pulse", "spark"],
     template_searchpath=["/opt/airflow/dags/repo/airflow/spark-apps"],
 ):
-    submit = SparkKubernetesOperator(
-        task_id="submit_spark_pi",
-        namespace="pizza-pulse",
-        application_file="spark-pi.yaml",
-        kubernetes_conn_id="kubernetes_default",
+    with TaskGroup(group_id="spark_pi") as spark_pi:
+        submit = SparkKubernetesOperator(
+            task_id="submit",
+            namespace="pizza-pulse",
+            application_file="spark-pi.yaml",
+            kubernetes_conn_id="kubernetes_default",
+            do_xcom_push=True,
+            random_name_suffix=True,
+            get_logs=False,
+            delete_on_termination=False,
+            reattach_on_restart=False,
+        )
 
-        # quan trọng
-        get_logs=False,
-        delete_on_termination=False,
-        do_xcom_push=False,
-        random_name_suffix=False,
-        reattach_on_restart=False,
-    )
+        monitor = SparkKubernetesSensor(
+            task_id="monitor",
+            namespace="pizza-pulse",
+            application_name="{{ ti.xcom_pull(task_ids='spark_pi.submit')['metadata']['name'] }}",
+            kubernetes_conn_id="kubernetes_default",
+            attach_log=True,
+        )
 
-    monitor = SparkKubernetesSensor(
-        task_id="monitor_spark_pi",
-        namespace="pizza-pulse",
-        application_name=APP_NAME,
-        kubernetes_conn_id="kubernetes_default",
-        attach_log=True,
-    )
-
-    submit >> monitor
+        submit >> monitor
