49d0cdb46523c62948c49f4c7c9dff74e94b51bf
thaihoc285 <thaihoc131005@gmail.com>
2026-05-19 23:46:27 +0700

    fix: update Spark operator DAG to set static application name and disable XCom push

--- airflow/dags/test_spark_operator.py
+++ airflow/dags/test_spark_operator.py
@@ -4,6 +4,8 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
+APP_NAME = "spark-pi-test"
+
 with DAG(
     dag_id="test_spark_operator",
     start_date=datetime(2026, 1, 1),
@@ -17,13 +19,19 @@ with DAG(
         namespace="pizza-pulse",
         application_file="spark-pi.yaml",
         kubernetes_conn_id="kubernetes_default",
-        do_xcom_push=True,
+
+        # quan trọng
+        get_logs=False,
+        delete_on_termination=False,
+        do_xcom_push=False,
+        random_name_suffix=False,
+        reattach_on_restart=False,
     )
 
     monitor = SparkKubernetesSensor(
         task_id="monitor_spark_pi",
         namespace="pizza-pulse",
-        application_name="{{ task_instance.xcom_pull(task_ids='submit_spark_pi')['metadata']['name'] }}",
+        application_name=APP_NAME,
         kubernetes_conn_id="kubernetes_default",
         attach_log=True,
     )
