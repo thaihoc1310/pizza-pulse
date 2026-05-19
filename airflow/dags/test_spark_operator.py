f2f3a56a3d372973dae40933eb06714892adfa2e
thaihoc285 <thaihoc131005@gmail.com>
2026-05-19 23:43:23 +0700

    fix: update Spark operator DAG to enable XCom push and use dynamic application name

--- airflow/dags/test_spark_operator.py
+++ airflow/dags/test_spark_operator.py
@@ -4,8 +4,6 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
-APP_NAME = "spark-pi-test"
-
 with DAG(
     dag_id="test_spark_operator",
     start_date=datetime(2026, 1, 1),
@@ -19,13 +17,13 @@ with DAG(
         namespace="pizza-pulse",
         application_file="spark-pi.yaml",
         kubernetes_conn_id="kubernetes_default",
-        do_xcom_push=False,
+        do_xcom_push=True,
     )
 
     monitor = SparkKubernetesSensor(
         task_id="monitor_spark_pi",
         namespace="pizza-pulse",
-        application_name=APP_NAME,
+        application_name="{{ task_instance.xcom_pull(task_ids='submit_spark_pi')['metadata']['name'] }}",
         kubernetes_conn_id="kubernetes_default",
         attach_log=True,
     )
