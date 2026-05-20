8fe30aa1f8d0d6c4808b69121b7dc562cd7a87ad
thaihoc285 <thaihoc131005@gmail.com>
2026-05-21 00:18:26 +0700

    fix: update mlflow helm value

--- airflow/dags/test_spark_operator.py
+++ airflow/dags/test_spark_operator.py
@@ -16,26 +16,16 @@ with DAG(
     tags=["pizza-pulse", "spark"],
     template_searchpath=["/opt/airflow/dags/repo/spark-apps"],
 ):
-    with TaskGroup(group_id="spark_pi") as spark_pi:
-        submit = SparkKubernetesOperator(
-            task_id="submit",
-            namespace="pizza-pulse",
-            application_file="spark-pi.yaml",
-            kubernetes_conn_id="kubernetes_default",
+    submit = SparkKubernetesOperator(
+        task_id="submit",
+        namespace="pizza-pulse",
+        application_file="spark-pi.yaml",
+        kubernetes_conn_id="kubernetes_default",
 
-            do_xcom_push=False,
-            random_name_suffix=False,
-            get_logs=False,
-            delete_on_termination=False,
-            reattach_on_restart=False,
-        )
+        do_xcom_push=False,
+        random_name_suffix=False,
+        get_logs=True,
+        delete_on_termination=False,
+        reattach_on_restart=False,
+    )
 
-        monitor = SparkKubernetesSensor(
-            task_id="monitor",
-            namespace="pizza-pulse",
-            application_name=APP_NAME,
-            kubernetes_conn_id="kubernetes_default",
-            attach_log=True,
-        )
-
-        submit >> monitor
